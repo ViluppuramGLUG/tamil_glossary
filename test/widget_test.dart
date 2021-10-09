fc8fc88f2ac2ea9f53c0e76a0211ac82487e4044
Khaleel Jageer <jskcse4@gmail.com>
2021-10-09 13:12:14 +0530

    Base setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:tamil_glossary/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GlossaryApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
